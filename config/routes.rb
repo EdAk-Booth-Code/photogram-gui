cc6c0dc96bea84dc68160c0a01677026f52cf3e0
EdAk-Booth <edwardacosta@chicagobooth.edu>
2021-06-04 08:31:25 +0000

    Created photos index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
 
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:username", {:controller => "users", :action => "show_user"})
+
+  get("/photos", { :controller => "photos", :action => "index"})
+
 
 end
